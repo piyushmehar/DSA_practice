08a7c166f865510859c4742e4e74fa246431875b
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-04-15 20:20:06 +0530

    Create Cells with Odd Values in a Matrix.java
    There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
    For each location indices[i], do both of the following:
    Increment all the cells on row ri.
    Increment all the cells on column ci.
    Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
    
     
    
    Example 1:
    
    
    Input: m = 2, n = 3, indices = [[0,1],[1,1]]
    Output: 6
    Explanation: Initial matrix = [[0,0,0],[0,0,0]].
    After applying first increment it becomes [[1,2,1],[0,1,0]].
    The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.

---
+++ Array/Cells with Odd Values in a Matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] old) {
+        
+        int[][] naya =new int[m][n];
+        int x=0;
+
+        for(int i=0;i<old.length;i++){
+            // x=0;
+            for(int j=0;j<n;j++){
+                naya[old[i][0]][j]++;
+            }
+            for(int j=0;j<m;j++){
+                naya[j][old[i][1]]++;
+            }
+        }
+        for(int i =0; i <naya.length;i++){
+            for(int j=0;j<naya[i].length;j++){
+                if(naya[i][j]%2!=0){
+                    x++;
+                }
+            }
+        }
+        return x;
+    }
+}
