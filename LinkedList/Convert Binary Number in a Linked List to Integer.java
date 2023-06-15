fca52ea89cf607f02c335c79cd8efb6aeb6adc0a
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-06-15 23:35:12 +0530

    Create Convert Binary Number in a Linked List to Integer.java

---
+++ LinkedList/Convert Binary Number in a Linked List to Integer.java
@@ -0,0 +1,16 @@
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        ListNode temp = head;
+        while(temp != null){
+            sb.append(temp.val);
+            temp = temp.next;
+        }
+        return Integer.parseInt(sb.toString(),2);
+    }
+}
