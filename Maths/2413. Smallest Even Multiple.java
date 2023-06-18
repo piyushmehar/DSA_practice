b92453a508ede540acbc149f7df111149e4a0122
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-06-18 15:16:08 +0530

    Create 2413. Smallest Even Multiple.java

---
+++ Maths/2413. Smallest Even Multiple.java
@@ -0,0 +1,20 @@
+// Example 1:
+
+// Input: n = 5
+// Output: 10
+// Explanation: The smallest multiple of both 5 and 2 is 10.
+// Example 2:
+
+// Input: n = 6
+// Output: 6
+// Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }else{
+            return 2*n;
+        }
+    }
+}
